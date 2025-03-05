d44c9e44ee25e643f9c4ef05d64074f115b1da08
Mike Brockschmidt <info@mike-brockschmidt.de>
2025-03-05 11:06:31 +0100

    if-else

---
+++ Aufgabe1.dart
@@ -0,0 +1,17 @@
+enum Weekday { monday, tuesday, wednesday, thursday, friday, saturday, sunday }
+
+void main() {
+  Weekday? weekday;
+
+  if (weekday == Weekday.monday || weekday == Weekday.tuesday) {
+    print('Montage und Dienstage sind anstrengend');
+  } else if (weekday == Weekday.wednesday || weekday == Weekday.thursday) {
+    print('Mittwoch und Donnerstag sind ok');
+  } else if (weekday == Weekday.friday) {
+    print('Freitag ist super');
+  } else if (weekday == Weekday.saturday || weekday == Weekday.sunday) {
+    print('Wochenende ist genial');
+  } else {
+    print('Kein Tag ausgewählt');
+  }
+}
