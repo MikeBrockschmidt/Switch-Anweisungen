bcb70a83708e1ed28e15ca677876b469551774f9
Mike Brockschmidt <info@mike-brockschmidt.de>
2025-03-05 11:50:34 +0100

    1

---
+++ Aufgabe2.dart
@@ -0,0 +1,20 @@
+switch (sex) {
+   case _____:
+     switch (age) {
+       case _____:
+         print("Im Schnitt 181,4m");
+       case _____:
+         print("Im Schnitt 181,3m");
+       case _____:
+         print("Im Schnitt 180,4m");
+     }
+   case _____:
+     switch (age) {
+       case _____:
+         print("Im Schnitt 167,5m");
+       case _____:
+         print("Im Schnitt 167,3 m");
+       case _____:
+         print("Im Schnitt 167,2");
+     }
+ }
